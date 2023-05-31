348fa4d4704058b3e54b75b46e0b4092b25fa1ce
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-05-31 13:10:04 -0600

    Added the main module to dispatch the functions to the right adapter

--- lib/any_json.ex
+++ lib/any_json.ex
@@ -3,16 +3,42 @@ defmodule AnyJson do
   Documentation for `AnyJson`.
   """
 
-  @doc """
-  Hello world.
+  ## Behaviours
 
-  ## Examples
+  @behaviour AnyJson.Codec
 
-      iex> AnyJson.hello()
-      :world
+  ## Public functions
 
-  """
-  def hello do
-    :world
+  @impl true
+  @spec encode(any(), keyword()) :: {:ok, String.t()} | {:error, any()}
+  def encode(value, opts \\ []), do: adapter().encode(value, opts)
+
+  @impl true
+  @spec encode!(any(), keyword()) :: String.t()
+  def encode!(value, opts \\ []), do: adapter().encode!(value, opts)
+
+  @impl true
+  @spec decode(String.t(), keyword()) :: {:ok, any()} | {:error, any()}
+  def decode(value, opts \\ []), do: adapter().decode(value, opts)
+
+  @impl true
+  @spec decode!(String.t(), keyword()) :: any()
+  def decode!(value, opts \\ []), do: adapter().decode!(value, opts)
+
+  # Defines the adapter function which can checked at compilation time.
+  if Application.compile_env(:any_json, :compiled_adapter, false) do
+    @adapter Application.compile_env(:any_json, :json_adapter)
+
+    if is_nil(@adapter) do
+      raise("Please set the `:json_adapter` configuration option in your `config.exs` file.")
+    end
+
+    @doc false
+    @spec adapter() :: module()
+    def adapter, do: @adapter
+  else
+    @doc false
+    @spec adapter() :: module()
+    def adapter, do: Application.get_env(:any_json, :json_adapter, AnyJson.Jason)
   end
 end
--- test/any_json_test.exs
+++ test/any_json_test.exs
@@ -1,8 +1,41 @@
 defmodule AnyJsonTest do
-  use ExUnit.Case
-  doctest AnyJson
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert AnyJson.hello() == :world
+  import Mox, only: [expect: 3, verify_on_exit!: 1]
+
+  alias AnyJson, as: Subject
+
+  describe "adapter/0" do
+    test "returns AnyJsonMock" do
+      assert Subject.adapter() == AnyJsonMock
+    end
+  end
+
+  describe "with mocked adapter" do
+    setup :verify_on_exit!
+
+    test "encode/2 calls the adapter" do
+      expect(AnyJsonMock, :encode, fn %{hello: 42}, [custom: :option] -> {:ok, "encoded"} end)
+
+      assert Subject.encode(%{hello: 42}, custom: :option) == {:ok, "encoded"}
+    end
+
+    test "encode!/2 calls the adapter" do
+      expect(AnyJsonMock, :encode!, fn %{hello: 42}, [custom: :option] -> "encoded" end)
+
+      assert Subject.encode!(%{hello: 42}, custom: :option) == "encoded"
+    end
+
+    test "decode/2 calls the adapter" do
+      expect(AnyJsonMock, :decode, fn %{hello: 42}, [custom: :option] -> {:ok, "decoded"} end)
+
+      assert Subject.decode(%{hello: 42}, custom: :option) == {:ok, "decoded"}
+    end
+
+    test "decode!/2 calls the adapter" do
+      expect(AnyJsonMock, :decode!, fn %{hello: 42}, [custom: :option] -> "decoded" end)
+
+      assert Subject.decode!(%{hello: 42}, custom: :option) == "decoded"
+    end
   end
 end
